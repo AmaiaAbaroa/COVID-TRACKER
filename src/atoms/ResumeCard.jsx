951923e4aac8d11cfcdc92354e09d51d2a40bc1d
SoleGrobas <solegrobas@yahoo.com.ar>
2023-06-26 14:47:08 +0200

    ApiService

---
+++ src/atoms/ResumeCard.jsx
@@ -0,0 +1,26 @@
+import './ResumCard.css'
+
+
+const ResumCard = (props) => {
+
+  //<h3>{props.cardTitle} : {props.todayCases}</h3>
+
+
+ 
+
+  return (
+    <div>
+  
+        <div>
+          <div className= {props.card_resum_style}>
+           <h4>{props.card_resum_title}</h4>
+           <div className='card_resum_chip'>{props.card_resum_chip_value}</div>
+           <div className={props.card_resum_number_style}>{props.card_resum_number_value}</div>
+          </div>
+        </div>
+     
+    </div>
+  );
+}     
+
+export default ResumCard;
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,9 +1,16 @@
 import CovidData from "../atoms/TopTen"
+import UseFetch from "../services/UseFetch"
 
 function Landingpage(){
+    const url = "https://disease.sh/v3/covid-19/countries";
+    
     return(
         <>
-            <h1>HOLA HOLITA</h1>
+            <h1>Listado</h1>
+            <ul>
+                {data?.map((element)=>
+                <li key={element.name}>{element.country}</li>)}
+            </ul>
             <CovidData></CovidData>
         </>
     )
