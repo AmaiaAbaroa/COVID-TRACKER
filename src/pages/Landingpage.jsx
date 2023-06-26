38e85e2a3631ab4ea9d8d718ecfcb459960f42fe
Anais-RV <anaisrv@gmail.com>
2023-06-26 14:31:21 +0200

    service

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,10 +1,19 @@
-import CovidData from "../atoms/TopTen"
+//import CovidData from "../atoms/TopTen"
+import UseFetch from "../services/ApiService"
 
 function Landingpage(){
+
+    const url = "https://disease.sh/v3/covid-19/countries";
+    const {data} = UseFetch(url);
+
     return(
         <>
             <h1>HOLA HOLITA</h1>
-            <CovidData></CovidData>
+           {/* <CovidData></CovidData>*/}
+           <ul>
+            {data?.map{element}=>
+            <li key={element.name}>element.country}</li>)}
+           </ul>
         </>
     )
 }
