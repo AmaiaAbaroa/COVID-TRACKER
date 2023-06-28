171ec7c2369d68ea3a2976ffe15e678228079a3b
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-06-28 10:54:17 +0200

    Service OK

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -6,7 +6,7 @@ import Cards from "../atoms/Cards/Cards"
 function Landingpage(){
     const url = 'https://disease.sh/v3/covid-19/';
     const {data} = UseFetch(url + 'countries');
-    
+       
     return(
         <> 
             <h1>HOLA HOLITA</h1>
@@ -22,17 +22,6 @@ function Landingpage(){
             </Cards>
             )
         }    
-            {data?.map((element)=> 
-            <Cards 
-            key={element.cases} 
-            card_resum_title={element.country} 
-            card_resum_style={"card_resum_lightblue"} 
-            card_resum_chip_value={element.todayCases}
-            card_resum_number_style={"card_resum_number_red"}
-            card_resum_number_value={element.cases}> 
-            </Cards>
-    )
-}
           
            
         </>
--- src/pages/tracker2/Tracker2.jsx
+++ src/pages/tracker2/Tracker2.jsx
@@ -1,28 +1,41 @@
 import UseFetch from "../../services/UseFetch"
-import Cards from "../../atoms/Cards/Cards"
+import Cards from "../../atoms/Cards/Cards";
 
 
 function Tracker2(){
     const url = 'https://disease.sh/v3/covid-19/';
-    const {data} = UseFetch(url + 'countries/spain?strict=true');
-
+    const {data} = UseFetch(url + 'countries/spain');
+    const {data: dataFrance} = UseFetch(url + 'countries/france');
+    console.log(data)
+    
+    if (data === null){
+        return <> Loading </>
+    }
     return(
-        <> 
-            
-            {data?.map((spain)=> 
+        <>      
+            <Cards 
+            key= {'spain'}
+            card_resum_title={data.country} 
+            card_resum_chip_value={data.todayCases}
+            card_resum_number_value={data.cases}
+            card_resum_number_style={"card_resum_number_red"}
+            card_resum_style={"card_resum_lightblue"}>
+
+            </Cards>
 
             <Cards 
-            key={spain.cases} 
-            card_resum_title={spain.country} 
-            card_resum_style={"card_resum_lightblue"} 
-            card_resum_chip_value={spain.todayCases}
+            key= {'france'}
+            card_resum_title={dataFrance.countryInfo} 
+            card_resum_chip_value={dataFrance.critical}
+            card_resum_number_value={dataFrance.cases}
             card_resum_number_style={"card_resum_number_red"}
-            card_resum_number_value={spain.cases}> 
-            </Cards>
-       
-         )
-        }
-          
+            card_resum_style={"card_resum_lightblue"}>
+
+            </Cards> 
+            {/* // card_resum_title={data.country} 
+            // card_resum_chip_value={data.todayCases}
+            // card_resum_number_style={"card_resum_number_red"}
+            // card_resum_number_value={data.cases}  */}
            
         </>
     )
