594ba5a5d52c58eeeb53221e64066b77d6eedc2b
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-06-29 12:48:47 +0200

    icon navbar online

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,8 +1,10 @@
 import CovidData from "../atoms/TopTen"
+import Navbar from "../molecules/Navbar"
 
 function Landingpage(){
     return(
         <>
+            <Navbar></Navbar>
             <h1>HOLA HOLITA</h1>
             <CovidData></CovidData>
         </>
